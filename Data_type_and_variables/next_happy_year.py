924c4a83d6c05b8c3eba01b480ef922786929801
hboyadzhiev <113359669+hboyadzhiev@users.noreply.github.com>
2022-09-26 17:21:45 +0300

    comments deleted
    comments deleted

--- Data_type_and_variables/next_happy_year.py
+++ Data_type_and_variables/next_happy_year.py
@@ -10,32 +10,3 @@ while stop == False:
 print(year)
 
 
-
-# year = int(input())
-#
-# next_year = year
-# final_year = 0
-#
-# stop = False
-# string_of_final_year = str(final_year)
-# set_of_final_year = set(string_of_final_year)
-#
-# while stop == False:
-#     final_year += 1
-#     lenght_of_next_year = len(set_of_final_year)
-#     lenght_of_year = len(str(year))
-#     if lenght_of_next_year == lenght_of_year:
-#         stop = True
-#         print(final_year)
-
-
-# year = 9013
-# lenght_of_year = len(set(str(year)))
-# print(lenght_of_year)
-
-# year1 = len(set(str(year)))
-# print(year1)
-# print(year1)
-#
-# # year = 1998
-# years = 2000
