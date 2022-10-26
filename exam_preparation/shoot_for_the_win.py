130362c860d24d11a7e3fc10d011edf89d9509b3
hboyadzhiev <113359669+hboyadzhiev@users.noreply.github.com>
2022-10-26 12:36:51 +0300

    exercise completed
    exercise completed

--- exam_preparation/shoot_for_the_win.py
+++ exam_preparation/shoot_for_the_win.py
@@ -1,7 +1,6 @@
 targets = input().split(" ")
 target_sequence = [int(x) for x in targets]
 count_of_shot_targets = 0
-current_target = 0
 
 def if_bigger_smaller(some_integer):
     if some_integer > current_target and some_integer != -1:
@@ -15,13 +14,13 @@ while True:
     if command == "End":
         break
     current_index = int(command)
+    current_target = 0
     if current_index < len(target_sequence):
         if target_sequence[current_index] != -1 and -1 < current_index:
             count_of_shot_targets += 1
             current_target = int(target_sequence[current_index])
             target_sequence[current_index] = -1
             target_sequence = list(map(if_bigger_smaller, target_sequence))
-            current_target = 0
 
 int_into_strings = [str(x) for x in target_sequence]
 print(f"Shot targets: {count_of_shot_targets} -> {' '.join(int_into_strings)}")
