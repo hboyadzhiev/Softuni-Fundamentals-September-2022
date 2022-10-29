abf98768ef54c617d3d168878e2909f45e9c6114
hboyadzhiev <113359669+hboyadzhiev@users.noreply.github.com>
2022-10-29 22:20:19 +0300

    exercise completed
    exercise completed

---
+++ exam_preparation/inventory.py
@@ -0,0 +1,21 @@
+items = input().split(", ")
+
+def commands(current_command):
+    splitted_command = current_command.split(" - ")
+    action = splitted_command[0]
+    if action == "Collect":
+        pass
+    elif action == "Drop":
+        pass
+    elif action == "Combine Items":
+        pass
+    elif action == "Renew":
+        pass
+
+while True:
+    command = input()
+    if command == "Craft!":
+        break
+
+print(", ".join())
+
