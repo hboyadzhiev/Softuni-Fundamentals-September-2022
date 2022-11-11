1149e2716d326ecce5aee2ac373f85745a3c47d8
hboyadzhiev <113359669+hboyadzhiev@users.noreply.github.com>
2022-11-11 11:31:52 +0200

    exercise edited
    exercise edited

--- exercise_dictionaries/capitals.py
+++ exercise_dictionaries/capitals.py
@@ -1,6 +1,7 @@
 countries = input().split(", ")
 cities = input().split(", ")
-our_dictionary = {countries[index]: cities[index] for index in range(len(countries))}
+# our_dictionary = {countries[index]: cities[index] for index in range(len(countries))}
+our_dictionary = {country: city for country, city in zip(countries, cities)}
 for key, value in our_dictionary.items():
     print(f"{key} -> {value}")
 
