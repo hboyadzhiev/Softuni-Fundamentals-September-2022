bae2f46a79878d71cc20d3630cfdcc141163ce06
hboyadzhiev <113359669+hboyadzhiev@users.noreply.github.com>
2022-11-18 20:25:36 +0200

    exercise completed
    exercise completed

---
+++ exercise_text_processing/extract_file.py
@@ -0,0 +1,9 @@
+file = input().split("\\")
+file_name = file[-1].split(".")[0]
+file_extension = file[-1].split(".")[1]
+
+print(f"File name: {file_name}")
+print(f"File extension: {file_extension}")
+
+
+
