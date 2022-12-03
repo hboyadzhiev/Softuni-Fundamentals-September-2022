f83477ce9d6dd8b509b12cc519a7871c743aae8d
hboyadzhiev <113359669+hboyadzhiev@users.noreply.github.com>
2022-12-03 18:54:28 +0200

    exercise completed
    exercise completed

--- programming_fundamentals_with_python_exam_preparation/mirror_words.py
+++ programming_fundamentals_with_python_exam_preparation/mirror_words.py
@@ -1,6 +1,48 @@
 import re
+
 string = input()
+mirror_words = []
 pattern = r"([\#\@])([A-Za-z]{3,})(\1{2})([A-Za-z]{3,})(\1)"
 matches = re.findall(pattern, string)
-for match in matches:
-    
+pairs_count = len(matches)
+if pairs_count == 0:
+    print("No word pairs found!")
+else:
+    print(f"{pairs_count} word pairs found!")
+    for match in matches:
+        if match[1] == match[3][::-1]:
+            mirror_words.append(f"{match[1]} <=> {match[3]}")
+
+if len(mirror_words) == 0:
+    print("No mirror words!")
+else:
+    print("The mirror words are:")
+    print(", ".join(mirror_words))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
